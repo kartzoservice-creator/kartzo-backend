ffa5d4f5946433737812e32e397d6e3501e3e6c8
kartzoservice-creator <kartzoservice@gmail.com>
2025-10-25 14:45:44 +0530

    Debug: Add console.log for MONGODB_URI

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // Database connection
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mera-database');
 
 mongoose.connection.on('connected', () => {
